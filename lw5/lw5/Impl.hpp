af78828eef95aa0045c992edad8cb2a669a59f27
eligantRU <eligant.ru@gmail.com>
2019-06-16 00:29:17 +0400

    Init commit for snowdrifts(lw5)

---
+++ lw5/lw5/Impl.hpp
@@ -0,0 +1,96 @@
+#include <algorithm>
+#include <iostream>
+#include <numeric>
+#include <vector>
+
+using namespace::std;
+
+template <class T>
+class SegmentTree
+{
+public:
+	explicit SegmentTree(const vector<T> & base)
+	{
+		auto tmp(base);
+
+		m_crutch = !(tmp.size() % 4) ? 0 : 4 - (tmp.size() % 4);
+		tmp.insert(tmp.begin(), m_crutch, 0);
+		m_tree = vector<T>(4 * tmp.size(), 0);
+		BuildImpl(tmp, 1, 0, tmp.size() - 1);
+		m_size = tmp.size();
+	}
+
+	T Sum(size_t left, size_t right) const
+	{
+		T result = 0;
+		for (size_t pos = left + m_crutch; pos < right + m_crutch; ++pos)
+		{
+			result += Get(1, 0, m_size, pos + 1);
+		}
+		return result;
+	}
+
+	void Add(size_t left, size_t right, T value)
+	{
+		AddImpl(1, 0, m_size, left + m_crutch, right + m_crutch, value);
+	}
+
+	void Print(ostream & strm = cout) const
+	{
+		for (size_t pos = m_crutch; pos < m_size; ++pos)
+		{
+			strm << Get(1, 0, m_size, pos + 1) << " ";
+		}
+		strm << endl;
+	}
+
+private:
+	void BuildImpl(const vector<T> & base, size_t v, size_t left, size_t right)
+	{
+		if (left == right)
+		{
+			m_tree[v] = base[left];
+		}
+		else
+		{
+			const auto middle = (left + right) / 2;
+			BuildImpl(base, 2 * v, left, middle);
+			BuildImpl(base, 2 * v + 1, middle + 1, right);
+		}
+	}
+
+	void AddImpl(size_t v, size_t left, size_t right, size_t l, size_t r, T addValue)
+	{
+		if (l > r)
+		{
+			return;
+		}
+
+		if (l == left && right == r)
+		{
+			m_tree[v] += addValue;
+		}
+		else
+		{
+			const auto middle = (left + right) / 2;
+			AddImpl(2 * v, left, middle, l, min(r, middle), addValue);
+			AddImpl(2 * v + 1, middle + 1, right, max(l, middle + 1), r, addValue);
+		}
+	}
+
+	T Get(size_t v, size_t left, size_t right, size_t pos) const
+	{
+		if (left == right)
+		{
+			return m_tree[v];
+		}
+		const auto middle = (left + right) / 2;
+		return (pos <= middle)
+			? m_tree[v] + Get(2 * v, left, middle, pos)
+			: m_tree[v] + Get(2 * v + 1, middle + 1, right, pos);
+	}
+
+	vector<T> m_tree;
+	size_t m_crutch = 0;
+	size_t m_size = 0;
+};
---
+++ lw5/lw5/main.cpp
@@ -0,0 +1,36 @@
+﻿/*
+16.4. Сугробы на ЛЭП (8)
+Служба электроснабжения проводит мониторинг уровня снега, лежащего на ЛЭП. Вся ЛЭП
+разбивается на участки опорами. Если снег падает на некоторый интервал ЛЭП, то высота
+снежного покрова на этом интервале увеличивается на высоту выпавшего снега. Снег также имеет
+тенденцию таять на некотором участке трассы в результате оттепели, однако сугробов
+отрицательной толщины быть не может. Энергетикам крайне важно уметь узнавать суммарную
+высоту снежного покрова на некоторых участках, чтобы определять вероятность обрыва проводов.
+
+Егоров Никита, ПС-21
+*/
+
+#include "Impl.hpp"
+
+int main()
+{
+	size_t pillarsCount, commandsCount;
+	cin >> pillarsCount >> commandsCount;
+
+	SegmentTree<int64_t> tree(vector<int64_t>(pillarsCount - 1, 0));
+	for (size_t i = 0; i < commandsCount; ++i)
+	{
+		size_t commandType, leftPillar, rightPillar;
+		cin >> commandType >> leftPillar >> rightPillar;
+		if (commandType == 1)
+		{
+			int val;
+			cin >> val;
+			tree.Add(leftPillar, rightPillar - 1, val);
+		}
+		else if (commandType == 2)
+		{
+			cout << tree.Sum(leftPillar - 1, rightPillar - 1) << endl;
+		}
+	}
+}
